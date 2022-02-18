3eaa3c06691d91f498ef4d9eb76b9563d2de035e
Dany Sluijk <me@dany.dev>
2022-02-18 13:54:34 +0100

    feat: change logo, add favicon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { h, FunctionalComponent } from "preact";
 
 import * as style from "./style.scss";
 
-import icon from "url:/static/logo.png?as=webp&width=500";
+import icon from "url:/static/logo_alt.jpg?as=webp&width=500";
 
 /**
  * Renders the header of the site.
